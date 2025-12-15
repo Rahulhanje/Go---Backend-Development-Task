fad3fddbe298ba4527d5674b6c2781f83c3ade1a
Rahul <rahulhanje0.7@gmail.com>
2025-12-15 20:51:06 +0530

    added sqlc queries

---
+++ db/sqlc/query.sql
@@ -0,0 +1,22 @@
+-- name: CreateUser :one
+INSERT INTO users (name, dob)
+VALUES ($1, $2)
+RETURNING *;
+
+-- name: GetUserByID :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: UpdateUser :one
+UPDATE users
+SET name = $2, dob = $3
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE id = $1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY id;
